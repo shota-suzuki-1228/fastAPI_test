87f3eeefe282bd69095e31f55426a9f3240bfb15
鈴木翔大 <2020laxprojectteam@gmail.com>
2025-10-18 02:15:18 +0900

    Fieldの追加

--- schemas/contact.py
+++ schemas/contact.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel,Field
+from pydantic import BaseModel, EmailStr,Field, HttpUrl
 from datetime import datetime
 
 
 class Contact(BaseModel):
-    id: int
-    name: str
-    email: str
-    url: str
-    gender: str
-    message: str
-    is_enabled: bool
+    id: int 
+    name: str = Field(..., min_length=2, max_length=50)
+    email: EmailStr
+    url: HttpUrl | None = Field(default=None)
+    gender: int = Field(..., strict=True, ge=0, le=2)
+    message: str = Field(..., max_length=200)
+    is_enabled: bool = Field(default=False)
     create_at: datetime
