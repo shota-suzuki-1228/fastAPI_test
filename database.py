48c71ea0d8c5f2fc6b3323e6dc0e46b3d1056edf
鈴木翔大 <2020laxprojectteam@gmail.com>
2025-10-18 17:44:28 +0900

    database.pyの作成

---
+++ database.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./fast_api_app.db"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,echo=True
+)
+
+Base = declarative_base()
+
+db_session = sessionmaker(
+    autocommit = False, 
+    autoflush=False, 
+    bind=engine,
+    class_=AsyncSession
+    )
+
+async def get_db():
+    async with db_session as session:
+        yield session
