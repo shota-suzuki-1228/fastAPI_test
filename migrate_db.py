b947aef6777c7c767a93ea1501e3199a85d2cceb
鈴木翔大 <2020laxprojectteam@gmail.com>
2025-10-19 03:38:41 +0900

    migrationの実行とファイルの修正

--- migrate_db.py
+++ migrate_db.py
@@ -7,9 +7,9 @@ DB_URL = "sqlite+aiosqlite:///fastapi-app.db"
 engine = create_async_engine(DB_URL, echo=True)
 
 async def reset_database():
- async with engine.begin() as conn: # with 例外発生時でもリソース解放される
-  await conn.run_sync(Base.metadata.drop_all) # テーブル削除
-  await conn.run_sync(Base.metadata.create_all) # テーブル作成
-  
-if __name__ == "__main__": # スクリプトで実行時のみ実行
+ async with engine.begin() as conn: 
+  await conn.run_sync(Base.metadata.drop_all) 
+  await conn.run_sync(Base.metadata.create_all) 
+
+if __name__ == "__main__": 
  asyncio.run(reset_database())
