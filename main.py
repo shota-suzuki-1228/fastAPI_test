d3a09a39f0a85c9e3ad0536f26cf7b7e298ca036
鈴木翔大 <2020laxprojectteam@gmail.com>
2025-10-20 21:40:12 +0900

    schemasモデルの修正

--- main.py
+++ main.py
@@ -3,8 +3,4 @@ from routers import contact
 
 app = FastAPI()
 
-# @app.get("/")
-# async def read_hello():
-    # return {"response":"Hello! World!"}
-
 app.include_router(contact.router)
--- schemas/contact.py
+++ schemas/contact.py
@@ -2,12 +2,34 @@ from pydantic import BaseModel, EmailStr,Field, HttpUrl
 from datetime import datetime
 
 
-class Contact(BaseModel):
-    id: int 
+class ContactList(BaseModel):
+    id: int
+    name: str = Field(..., min_length=2, max_length=50)
+    create_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class ContactBase(BaseModel):
     name: str = Field(..., min_length=2, max_length=50)
     email: EmailStr
     url: HttpUrl | None = Field(default=None)
     gender: int = Field(..., strict=True, ge=0, le=2)
     message: str = Field(..., max_length=200)
     is_enabled: bool = Field(default=False)
+
+    class Config:
+        from_attributes = True
+
+
+class ContactDelatil(ContactBase):
+    id: int
     create_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class ContactCreate(ContactBase):
+    pass
