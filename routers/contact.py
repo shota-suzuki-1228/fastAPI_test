92f47d1807c67e616e8a9efb2428101891481793
鈴木翔大 <2020laxprojectteam@gmail.com>
2025-10-21 10:02:16 +0900

    routersの編集

--- routers/contact.py
+++ routers/contact.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+import cruds.contact as contact_crud
 import schemas.contact as contact_schema
+from database import get_db
 from datetime import datetime
 
 router = APIRouter()
@@ -13,9 +16,9 @@ async def lead_contact_all():
         create_at= dummy_data
         )]
 
-@router.post("/contacts", response_model=contact_schema.Contact)
-async def create_contact(body: contact_schema.Contact):
-    return contact_schema.Contact(**body.model_dump())
+@router.post("/contacts", response_model=contact_schema.ContactCreate)
+async def create_contact(body: contact_schema.ContactCreate, db: AsyncSession = Depends(get_db)):
+    return await contact_crud.create_contact(db=db, contact=body)
     
 
 @router.get("/contacts/{id}")
