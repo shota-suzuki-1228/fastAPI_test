1555b1c4fb6cb3d0c426b2b895491a34881c936a
鈴木翔大 <2020laxprojectteam@gmail.com>
2025-10-21 19:34:50 +0900

    全件取得処理の実装

--- cruds/contact.py
+++ cruds/contact.py
@@ -1,6 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 import schemas.contact as contact_schema
 import models.contact as contact_model
+from typing import List,Tuple
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from datetime import datetime
 
 async def create_contact(db: AsyncSession, contact: contact_schema.ContactCreate) -> contact_model.Contact:
     contact_data = contact.model_dump()
@@ -13,3 +17,13 @@ async def create_contact(db: AsyncSession, contact: contact_schema.ContactCreate
     await db.commit()
     await db.refresh(db_contact)
     return db_contact
+
+async def get_contact_all(db: AsyncSession) -> List[Tuple[int, str, datetime]]:
+    result : Result = await db.execute(
+        select(
+            contact_model.Contact.id,
+            contact_model.Contact.name,
+            contact_model.Contact.create_at
+        )
+    )
+    return result.all()
--- routers/contact.py
+++ routers/contact.py
@@ -8,13 +8,8 @@ from datetime import datetime
 router = APIRouter()
 
 @router.get("/contacts",response_model=list[contact_schema.ContactList])
-async def lead_contact_all():
-    dummy_data = datetime.now()
-    return [contact_schema.Contact(
-        id=1, name="スズキショウタ", email="aaa@gmail.com", url = "https://www.example-nonexistent-domain-xyz1234.com/this-page-does-not-exist", 
-        gender=2, message="テスト", is_enabled= True, 
-        create_at= dummy_data
-        )]
+async def lead_contact_all(db: AsyncSession = Depends(get_db)):
+    return await contact_crud.get_contact_all(db=db)
 
 @router.post("/contacts", response_model=contact_schema.ContactCreate)
 async def create_contact(body: contact_schema.ContactCreate, db: AsyncSession = Depends(get_db)):
